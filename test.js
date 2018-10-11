525beb57aeb8273fc958b388fbe4373fe0f75c52
Kayla Washburn <partheseas@gmail.com>
2018-10-11 15:51:44 -0600

    Oops, those were dev files

--- test.js
+++
@@ -1,9 +0,0 @@
-const path = require( 'path' )
-const zzz = require( './unzzz' )
-
-unzzz( path.join( __dirname, 'archive.zip' ) )
-  .then( archive => {
-    for ( each in archive.files ) {
-      archive.unzipFile( each )
-    }
-  })
